50a8f4987731ff3a016d1b644a779916e578577e
Suson Waiba <susonwaiba@gmail.com>
2020-10-05 19:32:07 +0545

    updated docker with variables and other changes

--- php7.4/fpm/docker-entrypoint.sh
+++ php7.4/fpm/docker-entrypoint.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-/root/manage-project-permission.sh
-
 if [ ${PHP_ENABLE_REDIS} -eq "1" ]; then
     docker-php-ext-enable redis
-    echo "-- php cli redis extension enabled"
+    echo "-- php fpm redis extension enabled"
 fi
 
 if [ ${PHP_ENABLE_XDEBUG} -eq "1" ]; then
     docker-php-ext-enable xdebug
-    echo "-- php cli xdebug extension enabled"
+    echo "-- php fpm xdebug extension enabled"
 fi
 
 if [ ${PHP_ENABLE_MONGODB} -eq "1" ]; then
     docker-php-ext-enable mongodb
-    echo "-- php cli mongodb extension enabled"
+    echo "-- php fpm mongodb extension enabled"
 fi
 
-tail -f /dev/null
+exec "$@"
